87bd26bdea26b31c47d091d586484af2cf30a805
Alemkhan <alemkhan.yergaliev@gmail.com>
2019-11-02 19:29:56 +0600

    test

--- tgbot.py
+++ tgbot.py
@@ -2,26 +2,13 @@ import telebot
 
 bot_token = "926752910:AAFGad8KGmXJGXkO2tX0re0MXLv4NPPLwTw"
 bot = telebot.TeleBot(bot_token)
+global logo = open('logo.jpg', 'rb')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Здравствуйте! Вы обратились в информационно-справочную службу 109')
+    bot.send_photo(message.chat.id,logo)
 
-@bot.message_handler(commands = ['help'])
-def start_message(message):
-    bot.send_message(message.chat.id, message.text)
 
-@bot.message_handler(regexp="SOME_REGEXP")
-def handle_message(message):
-	bot.send_message(message.chat.id, message.text)
-
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, message.text)
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text == 'Мирас':
-        bot.send_message(message.chat.id, 'Пидарас')
 
 bot.polling()
