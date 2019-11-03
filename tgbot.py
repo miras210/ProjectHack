20c0bf55e3626554bed1bad6d67600b48e34e878
Alemkhan <alemkhan.yergaliev@gmail.com>
2019-11-03 23:45:36 +0600

    Hello

--- tgbot.py
+++ tgbot.py
@@ -1,21 +1,181 @@
 import telebot
+import time
+import mysql.connector
 
 from telebot import types
 
-bot_token = "926752910:AAFGad8KGmXJGXkO2tX0re0MXLv4NPPLwTw"
-bot = telebot.TeleBot(bot_token)
-keyboardStart = telebot.types.ReplyKeyboardMarkup(True)
-Yes = types.KeyboardButton('Yes', request_contact=True)
-No = types.KeyboardButton('No', request_contact=False)
-keyboardStart.row(Yes,No)
+TOKEN = '926752910:AAFGad8KGmXJGXkO2tX0re0MXLv4NPPLwTw'
+
+knownUsers = []
+userStep = {}
+userNum = {}
+userName = {}
+userSurname = {}
+
+commands = {
+    'start'     : 'This is the beginning of ur journey',
+    'help'      : 'Use this in order to see other commands'
+}
+
+hideBoard = types.ReplyKeyboardRemove()
+
+def wrong_command(cid):
+    bot.send_message(cid, "Hey, bullsh*t you have typed wrong command")
+    bot.send_message(cid, "Please try again")
+
+def TelPhone(cid):
+    PAns = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    Yes = types.KeyboardButton('Yes', request_contact=True)
+    No = types.KeyboardButton('No', request_contact=False)
+    PAns.row(Yes, No)
+    bot.send_message(cid, "Do You trust us ur phone number ?", reply_markup=PAns)
+
+def get_user_data(uid):
+    print("[" + str(uid) + "] " + userName[uid] + " " + userSurname[uid] + " " + userNum[uid])
+    return userNum[uid]
+
+def get_user_step(uid):
+    if uid in userStep:
+        return userStep[uid]
+    else:
+        knownUsers.append(uid)
+        userStep[uid] = 0
+        print("New user detected and added \n")
+        return 0
+
+def listener(messages):
+    for m in messages:
+        print(str(m.chat.first_name) + " [" + str(m.chat.id) + "]: " + str(m.text))
+
+bot = telebot.TeleBot(TOKEN)
+bot.set_update_listener(listener)
+
+def alertBut(m):
+    cid = m.chat.id
+    time.sleep(1)
+    Ans = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    Ans.add('Yes', 'No')
+    bot.send_message(cid, "Is ur phone number in Telegram real one ?", reply_markup = Ans)
+    userStep[cid] = 1
+
+def PhoneCheck(num):
+    if not (len(num) == 11 or len(num) == 12):
+        return False
+    else:
+        if len(num) == 12 and (num[0] != '+' or num[1] != '7'):
+            return False
+        elif len(num) == 11 and num[0] != '7':
+            return False
+        else:
+            if len(num) == 12:
+                num = num[2:]
+            elif len(num) == 11:
+                num = num[1:]
+            for ch in num:
+                if not (ch >= '0' and ch <= '9'):
+                    return False
+            return True
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Здравствуйте! Вы обратились в информационно-справочную службу 109')
-    logo = open('logo.jpg', 'rb')
-    bot.send_photo(message.chat.id,logo)
-    bot.send_message(message.chat.id, 'Для дальнейшей работы с ботом нужен доступ к Вашим личным данным')
-    bot.send_message(message.chat.id, 'Согласны передать данные?:', reply_markup=keyboardStart)
-    reply_markup=telebot.types.ReplyKeyboardRemove(remove_keyboard=True)
+def command_help(m):
+    cid = m.chat.id
+    if cid not in knownUsers:
+        knownUsers.append(cid)
+        userStep[cid] = 0
+        bot.send_photo(cid, open('logo.jpg', 'rb'))
+        bot.send_message(cid, "Здравствуйте! Вы обратились в информационно-справочную службу 109")
+        alertBut(m)
+    else:
+        bot.send_message(cid, "I already know you, so what is ur choice ?")
+        #action buttons
 
-bot.polling(none_stop=True, interval=0, timeout=3)
+
+@bot.message_handler(func=lambda m: get_user_step(m.chat.id) == 1)
+def msg_choice(m):
+    cid = m.chat.id
+    text = m.text
+    if text == 'Yes':
+        userStep[cid] = 2
+        TelPhone(cid)
+    elif text == 'No':
+        userStep[cid] = 5
+        bot.send_message(cid, "Please enter ur phone number\nRight Format: +7**********")
+    else:
+        wrong_command(cid)
+
+@bot.message_handler(func=lambda m: get_user_step(m.chat.id) == 5)
+def inp_tel(m):
+    cid = m.chat.id
+    text = m.text
+    #func check num
+    if PhoneCheck(text):
+        if len(text) == 11:
+            userNum[cid] = text[1:]
+        elif len(text) == 12:
+            userNum[cid] = text[2:]
+        time.sleep(1)
+        bot.send_message(cid, "Enter your name, please")
+        userStep[cid] = 3
+    else:
+        bot.send_message(cid, "You have entered your phone number in a wrong format\nPlease try to do it again")
+
+@bot.message_handler(func=lambda m: get_user_step(m.chat.id) == 2)
+def phone_num(m):
+    cid = m.chat.id
+    text = m.text
+    if text == 'No':
+        bot.send_message(cid, "Fuck*ng bastard send ur motherfuck*ng Telegram number")
+        TelPhone(cid)
+    else:
+        wrong_command(cid)
+
+@bot.message_handler(content_types=['contact'])
+def phone(m):
+    cid = m.chat.id
+    text = m.text
+    num = m.contact.phone_number
+    userNum[cid] = num
+    bot.send_message(cid, "Processing ur data ...", reply_markup=hideBoard)
+    time.sleep(2)
+    bot.send_message(cid, "Enter your name, please")
+    userStep[cid] = 3
+
+@bot.message_handler(func=lambda m: get_user_step(m.chat.id) == 3)
+def name(m):
+    cid = m.chat.id
+    text = m.text
+    userName[cid] = text
+    bot.send_message(cid, "Processing your data ...")
+    time.sleep(1)
+    bot.send_message(cid, "Enter your surname, please")
+    userStep[cid] = 4
+
+@bot.message_handler(func=lambda m: get_user_step(m.chat.id) == 4)
+def surname(m):
+    cid = m.chat.id
+    text = m.text
+    userSurname = text
+    get_user_data(cid)
+    bot.send_message(cid, "Thank you for your answers )\nNow wait a sec until new command appears")
+    time.sleep(1)
+
+@bot.message_handler(func=lambda m: get_user_step(m.chat.id) == 3)
+def name(m):
+    cid = m.chat.id
+    text = m.text
+    userName[cid] = text
+    bot.send_message(cid, "Processing your data ...")
+    time.sleep(1)
+    bot.send_message(cid, "Enter your surname, please")
+    userStep[cid] = 4
+
+@bot.message_handler(func=lambda m: get_user_step(m.chat.id) == 4)
+def surname(m):
+    cid = m.chat.id
+    text = m.text
+    userSurname[cid] = text
+    get_user_data(cid)
+    bot.send_message(cid, "Thank you for your answers )\nNow wait a sec until new command appears")
+    time.sleep(1)
+
+bot.polling()
