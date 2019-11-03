23fc3030bca75e37efc92fe459c977eb926d2efc
Miras <sektor.miras@gmail.com>
2019-11-03 17:08:31 +0600

    changes

--- tgbot.py
+++ tgbot.py
@@ -4,7 +4,7 @@ from telebot import types
 
 bot_token = "926752910:AAFGad8KGmXJGXkO2tX0re0MXLv4NPPLwTw"
 bot = telebot.TeleBot(bot_token)
-keyboardStart = telebot.types.ReplyKeyboardMarkup(True)
+keyboardStart = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
 Yes = types.KeyboardButton('Yes', request_contact=True)
 No = types.KeyboardButton('No', request_contact=False)
 keyboardStart.row(Yes,No)
@@ -16,6 +16,5 @@ def start_message(message):
     bot.send_photo(message.chat.id,logo)
     bot.send_message(message.chat.id, 'Для дальнейшей работы с ботом нужен доступ к Вашим личным данным')
     bot.send_message(message.chat.id, 'Согласны передать данные?:', reply_markup=keyboardStart)
-    reply_markup=telebot.types.ReplyKeyboardRemove(remove_keyboard=True)
-
+    
 bot.polling(none_stop=True, interval=0, timeout=3)
